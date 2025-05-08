b52233169587a4b7f7726091fa547078fe3e8998
mcadamsx <richard.mcadams@amalitech.com>
2025-05-08 15:01:13 +0000

    feat: implement document rename and add new document

---
+++ src/components/header/DocumentName.tsx
@@ -0,0 +1,60 @@
+import { useEffect, useState } from "react";
+
+import IconDocument from "../../assets/icon-document.svg";
+import useDocumentStore from "../../lib/documentStore";
+
+export default function DocumentName(): JSX.Element {
+  const [documents, setDocuments, currentDocumentId] = useDocumentStore(
+    (state) => [state.documents, state.setDocuments, state.currentDocumentId],
+  );
+
+  const currentDocument = documents.find((doc) => doc.id === currentDocumentId);
+
+  const [documentName, setDocumentName] = useState(
+    currentDocument ? currentDocument.name : "",
+  );
+
+  useEffect(() => {
+    const updatedDocument = documents.find(
+      (doc) => doc.id === currentDocumentId,
+    );
+    if (updatedDocument) {
+      setDocumentName(updatedDocument.name);
+
+    }
+  }, [documents, currentDocumentId]);
+
+  const handleDocumentNameChange = (e: React.ChangeEvent<HTMLInputElement>) => {
+    setDocumentName(e.target.value);
+
+    // Update the current document's name
+    const updatedDocuments = documents.map((doc) => {
+      if (doc.id === currentDocumentId) {
+        return { ...doc, name: e.target.value };
+      }
+      return doc;
+    });
+    setDocuments(updatedDocuments);
+  };
+
+  return (
+    <div className="relative flex items-center overflow-x-auto">
+      <img src={IconDocument} alt="document icon" />
+      <div className="ml-4">
+        <label
+          className="hidden text-xs leading-none text-neutral-500 md:block"
+          htmlFor="document-name"
+        >
+          Document Name
+        </label>
+        <input
+          id="document-name"
+          type="text"
+          className="border-b border-b-transparent bg-transparent text-neutral-100 caret-orange outline-none focus:border-b-neutral-100"
+          value={documentName}
+          onChange={handleDocumentNameChange}
+        />
+      </div>
+    </div>
+  );
+}
---
+++ src/lib/documentStore.ts
@@ -0,0 +1,95 @@
+import { create } from "zustand";
+import { createJSONStorage, persist } from "zustand/middleware";
+
+import initialData from "../data/data.json";
+import { TDocument } from "../type";
+import { getDate } from "./utils";
+
+interface DocumentStore {
+  currentDocumentId: string;
+  setCurrentDocumentId: (currentDocument: string) => void;
+  documents: TDocument[];
+  setDocuments: (documents: TDocument[]) => void;
+  addDocument: (id: string) => void;
+  deleteDocument: () => void;
+  saveDocument: () => void;
+  displayPreviewOnly: boolean;
+  setDisplayPreviewOnly: (displayPreviewOnly: boolean) => void;
+}
+
+const useDocumentStore = create<DocumentStore>()(
+  persist(
+    (set) => ({
+      currentDocumentId: "2",
+      setCurrentDocumentId: (currentDocumentId: string) => {
+        set(() => ({ currentDocumentId }));
+      },
+      documents: initialData,
+      setDocuments: (documents: TDocument[]) => {
+        return set(() => ({ documents }));
+      },
+      addDocument: (id: string) => {
+        set((state) => ({
+          documents: [
+            ...state.documents,
+            {
+              id,
+              createdAt: getDate(),
+              name: "untitled-document.md",
+              content: "",
+            },
+          ],
+        }));
+      },
+      deleteDocument: () => {
+        set((state) => {
+          const currentDocumentId = state.currentDocumentId;
+          const filteredDocuments = state.documents.filter(
+            (doc) => doc.id !== currentDocumentId,
+          );
+          return {
+            documents: filteredDocuments,
+            currentDocumentId:
+              filteredDocuments.length > 0 ? filteredDocuments[0].id : "",
+          };
+        });
+      },
+      saveDocument: () => {
+        set((state) => {
+          const currentDocumentId = state.currentDocumentId;
+          const currentDocument = state.documents.find(
+            (doc) => doc.id === currentDocumentId,
+          );
+
+          if (currentDocument) {
+            const blob = new Blob([currentDocument.content], {
+              type: "text/plain",
+            });
+            const url = URL.createObjectURL(blob);
+            const a = document.createElement("a");
+            a.href = url;
+            a.download = currentDocument.name;
+            a.style.display = "none";
+
+            document.body.appendChild(a);
+            a.click();
+
+            document.body.removeChild(a);
+            URL.revokeObjectURL(url);
+          }
+          return { ...state };
+        });
+      },
+      displayPreviewOnly: false,
+      setDisplayPreviewOnly: (displayPreviewOnly: boolean) => {
+        set(() => ({ displayPreviewOnly }));
+      },
+    }),
+    {
+      name: "markdown-app-storage",
+      storage: createJSONStorage(() => sessionStorage),
+    },
+  ),
+);
+
+export default useDocumentStore;
