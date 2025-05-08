5f128380be87e75a8e51895e9905aa2138f9aa0b
mcadamsx <richard.mcadams@amalitech.com>
2025-05-08 14:56:46 +0000

    chore(project setup): installed packages and dependancies

---
+++ src/App.tsx
@@ -0,0 +1,47 @@
+import "react-toastify/dist/ReactToastify.css";
+
+import { ToastContainer } from "react-toastify";
+
+import Header from "./components/header/Header";
+import MarkdownEditor from "./components/MarkdownEditor";
+import Preview from "./components/Preview";
+import Sidebar from "./components/sidebar/Sidebar";
+import useDocumentStore from "./lib/documentStore";
+
+function App() {
+  const displayPreviewOnly = useDocumentStore(
+    (state) => state.displayPreviewOnly,
+  );
+
+  return (
+    <div className="wrapper grid transition-all">
+      <Sidebar />
+      <div>
+        <Header />
+        <main
+          className={`grid h-screen w-full ${
+            displayPreviewOnly ? "grid-cols-1" : "md:grid-cols-2"
+          }`}
+        >
+          <MarkdownEditor />
+          <Preview />
+        </main>
+      </div>
+
+      <ToastContainer
+        position="bottom-right"
+        autoClose={2000}
+        hideProgressBar
+        newestOnTop
+        closeOnClick
+        rtl={false}
+        pauseOnFocusLoss
+        draggable
+        pauseOnHover
+        bodyClassName="toastBody"
+      />
+    </div>
+  );
+}
+
+export default App;
